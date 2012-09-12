8ee7cb5b398f7e22a55b0601aa5fca4749526ff0
Josef Willenborg <willenborg@bbaw.de>
2012-09-12 15:45:28 +0000

    Verschiebung DocumentHandler ins Paket dochandler
    git-svn-id: http://telotadev-alt.bbaw.de/telotasvn/wsp/wspCms/trunk/wspCmsWebApp@428 4cd7446e-361a-11e1-ae49-e5e889259ed6

--- src/org/bbaw/wsp/cms/servlets/DocumentOperation.java
+++ src/org/bbaw/wsp/cms/servlets/DocumentOperation.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.bbaw.wsp.cms.document.DocumentHandler;
+import org.bbaw.wsp.cms.dochandler.DocumentHandler;
 import org.bbaw.wsp.cms.scheduler.CmsChainScheduler;
 import org.bbaw.wsp.cms.scheduler.CmsDocOperation;
 import org.bbaw.wsp.cms.servlets.util.ServletUtil;
