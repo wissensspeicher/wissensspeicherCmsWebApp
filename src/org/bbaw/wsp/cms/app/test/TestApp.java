c68154f0476c6279a9fc0a67a38b86a28918b43e
joey <willenborg@bbaw.de>
2013-01-23 15:08:14 +0100

    Vaadin Test App: Erstellung

---
+++ src/org/bbaw/wsp/cms/app/test/TestApp.java
@@ -0,0 +1,27 @@
+package org.bbaw.wsp.cms.app.test;
+
+import java.util.Date;
+
+import com.vaadin.Application;
+import com.vaadin.ui.*;
+import com.vaadin.ui.Button.ClickEvent;
+
+public class TestApp extends Application {
+  private static final long serialVersionUID = 1L;
+
+  @Override 
+  public void init() {
+    final Window mainWindow = new Window("Test Application");
+    Label label = new Label("Hello Test Application");
+    mainWindow.addComponent(label);
+    mainWindow.addComponent(
+      new Button("What is the time?",
+        new Button.ClickListener() {
+          public void buttonClick(ClickEvent event) {
+            mainWindow.showNotification(
+                "The time is " + new Date());
+          }
+       }));
+    setMainWindow(mainWindow);
+  }
+}
