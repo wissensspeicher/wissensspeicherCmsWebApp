12ee21fcf58c0a4f067b19d4bc5fb30ef53408e8
Marco Seidler <wsp-shk2@bbaw.de>
2013-05-28 16:54:03 +0200

    MySQL Funktionalität hinzugefügt

---
+++ src/org/bbaw/wsp/cms/servlets/RequestStatisticAnalyser.java
@@ -0,0 +1,24 @@
+package org.bbaw.wsp.cms.servlets;
+
+/**
+ * Class is used to split an
+ * 
+ * @author shk2
+ * 
+ */
+public class RequestStatisticAnalyser {
+	private final String request;
+
+	public RequestStatisticAnalyser(String request) {
+		this.request = request;
+	}
+
+	/**
+	 * Methode nimm Verbindung zu einer SQL Datenbank auf und speichert den Zeiger
+	 * lokal
+	 */
+	private void getDBConnection() {
+		// gehe rein tue nix
+	}
+
+}
