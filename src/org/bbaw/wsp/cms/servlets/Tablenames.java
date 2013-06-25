3d59f48b57891b0eea7319b82d7d3da0f390e9f5
Marco Seidler <wsp-shk2@bbaw.de>
2013-06-25 12:46:53 +0200

    ok Tablenames hat er irgendwie nciht erfasst gehabt

---
+++ src/org/bbaw/wsp/cms/servlets/Tablenames.java
@@ -0,0 +1,17 @@
+package org.bbaw.wsp.cms.servlets;
+
+public class Tablenames {
+
+    // Tablenames
+    public static final String QUERIES = "Queries";
+    public static final String QUERY_WORDS = "QueryWords";
+    public static final String RELEVANT_COUNTER = "RelevantCounter";
+    public static final String RELEVANT_DOCS = "RelevantDocs";
+
+    // column names for matching
+    public static final String QUERIES_COL = "query";
+    public static final String QUERY_WORDS_COL = "queryword";
+    public static final String RELEVANT_COUNTER_COL = "id_query";
+    public static final String RELEVANT_DOCS_COL = "uri";
+
+}
