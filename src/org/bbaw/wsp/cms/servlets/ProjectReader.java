89f248603cae2f6bc91d859b56d0a97567030ef9
joey <willenborg@bbaw.de>
2016-10-11 17:33:06 +0200

    Entwicklung der neuen Metadatenstruktur

--- src/org/bbaw/wsp/cms/servlets/ProjectReader.java
+++ src/org/bbaw/wsp/cms/servlets/ProjectReader.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.lucene.index.IndexableField;
-import org.apache.lucene.search.Query;
 import org.bbaw.wsp.cms.collections.Organization;
 import org.bbaw.wsp.cms.collections.Project;
 import org.bbaw.wsp.cms.collections.ProjectCollection;
